3c88e200f2c69ed1bef778e0704b11ed4d0d3073
JesseTNRoberts <34480689+JesseTNRoberts@users.noreply.github.com>
2023-07-18 14:16:22 -0500

    Update PopulationLM.py

--- PopulationLM.py
+++ PopulationLM.py
@@ -249,3 +249,7 @@ if __name__ == "__main__":
     print('mean')
     print(mean/len(logit_cat[cat]))
     print('-----')
+"""
+Adapted from work in:
+@inproceedings{shelmanov-etal-2021-certain, title = "How Certain is Your {T}ransformer?", author = "Shelmanov, Artem and Tsymbalov, Evgenii and Puzyrev, Dmitri and Fedyanin, Kirill and Panchenko, Alexander and Panov, Maxim", booktitle = "Proceedings of the 16th Conference of the European Chapter of the Association for Computational Linguistics: Main Volume", month = apr, year = "2021", address = "Online", publisher = "Association for Computational Linguistics", url = "https://www.aclweb.org/anthology/2021.eacl-main.157", pages = "1833--1840", abstract = "In this work, we consider the problem of uncertainty estimation for Transformer-based models. We investigate the applicability of uncertainty estimates based on dropout usage at the inference stage (Monte Carlo dropout). The series of experiments on natural language understanding tasks shows that the resulting uncertainty estimates improve the quality of detection of error-prone instances. Special attention is paid to the construction of computationally inexpensive estimates via Monte Carlo dropout and Determinantal Point Processes.", }
+"""
