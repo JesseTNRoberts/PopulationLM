c6783819dce1298f6034c2f72d2c018be1ea375c
JesseTNRoberts <34480689+JesseTNRoberts@users.noreply.github.com>
2024-03-13 09:34:41 -0500

    Update tools.py

--- PopulationLM/tools.py
+++ PopulationLM/tools.py
@@ -238,17 +238,18 @@ class DropoutUtils():
 
       # call the conversion method on the model
       replaced_layers = cls._convert_to_mc_dropout(model, replacement_dict)
+
+      print('replaced ', replaced_layers, ' layers.')
       
       if replaced_layers == 0:
         cls.show_model(model)
         print('trying to add dropout layers...')
         cls.add_new_dropout_layers(model)
-        replaced_layers = cls.convert_dropouts(model, stratified=stratified)
+        cls.convert_dropouts(model, stratified=stratified)
 
       if replaced_layers==0:
         raise ValueError("The number of converted layers is zero. This is because the model has no dropout layers. Add them using add_new_dropout_layers()")
       
-      print('replaced ', replaced_layers, ' layers.')
       
 
 def generate_dropout_population(model, call_to_model_lambda, committee_size = 20):
